eb487097a81dca2b0f32b8ee6c99b98aec7c5f07
Joel <joelarias_0@outlook.es>
2025-10-06 12:15:19 -0400

    Ruta para crear un nuevo curso

--- backend/src/routes/courses.js
+++ backend/src/routes/courses.js
@@ -1,5 +1,5 @@
 // 6 - routes/courses.js
-import { Router } from 'express';
+import e, { Router } from 'express';
 import Course from '../models/Course'
 import { CourseBodySchema } from '../schemas/course.schema';
 import { validate } from '../middlewares/validate';
@@ -9,9 +9,21 @@ const router = Router();
 // Obtener todos los cursos
 router.get('/', async (requestAnimationFrame, res) => {
     try {
-        const item = (await Course.find()).toSorted({ createdAt:-1}).lean();
+        const item = (await Course.find()).toSorted({ createdAt: -1 }).lean();
         res.json(item);
     } catch (error) {
         res.status(500).json({ error: 'Error al obtener los cursos' });
     }
 })
+
+// Crear un nuevo curso
+router.post('/', validate(CourseBodySchema), async (req, res) => {
+    try {
+        const doc = await Course.create(req.validated);
+        res.status(200).json(doc);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+})
+
+export default router;
